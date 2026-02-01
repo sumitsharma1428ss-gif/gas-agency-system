c4ded23107183f642de0d01453e5fcb528ce82d3
Sumit Sharma <sumitsharma1428.ss@gmail.com>
2026-02-01 12:49:23 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,102 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+let submit=document.querySelector(".btn");
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+function foo() {
+    alert("Successfully Login");
+    return true;
+ }
+
+  function login(form){
+    if((form.username.value == "test") && (form.password.value == "test")){
+      self.location.href = "login_dashboard.html";
+      }
+      else{
+        alert(" OOPs !! Incorrect email id and password");
+        return false;
+      }
+  }
+  
+  function admin_login(form){
+    if((form.email.value == "admin@gmail.com") && (form.password.value == "admin")){
+      self.location.href = "admin_dashboard.html";
+      }
+      else{
+        alert(" OOPs !! Incorrect email id and password");
+        return false;
+      }
+  }
+  const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
